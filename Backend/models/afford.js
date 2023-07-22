43b7a263f9bce3b994275800f5f4a1c9e13b32f9
Khaja zohebuddin <96819670+zoheb799@users.noreply.github.com>
2023-07-22 13:11:05 +0530

    Add files via upload

---
+++ Backend/models/afford.js
@@ -0,0 +1,31 @@
+const mongoose =  require('mongoose');
+const { stringify } = require('querystring');
+
+const multiSchema = new mongoose.Schema({
+    Name: {
+        type: String,
+        required: true
+    },
+    Ratings: {
+        type: Number,
+        required: true
+    },
+    Speciality: {
+        type: String,
+        required: true
+    },
+    Address: {
+        type: String,
+        required: true
+    },
+    Website: {
+        type:String,
+        required: true
+    },
+    Gmap: {
+        type: String,
+        required: true
+    }
+})
+const List = mongoose.model('afford', multiSchema);
+module.exports = List;
